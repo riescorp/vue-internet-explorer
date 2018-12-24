5431f174adaad0614f73bb80551c2a51419186b2
Nico R <riesco@gmail.com>
2018-12-24 17:24:32 -0300

    nono

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,8 @@ module.exports = {
       // modules: 'commonjs',
       useBuiltIns: 'entry'
     }]
-  ]
+  ],
+  plugins: ['@babel/plugin-transform-shorthand-properties']
 }
 // https://github.com/vuejs/vue-cli/issues/1568
 // https://github.com/vuejs/vue-cli/issues/2637#issuecomment-425403329
