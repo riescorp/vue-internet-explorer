6e2393a62d3e052dc44f9b8ab33894d079000cf3
Nico R <riesco@gmail.com>
2018-12-31 11:20:14 -0300

    real fix no polyfill

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 // import 'babel-polyfill'
-import '@babel/polyfill'
+// import '@babel/polyfill'
 
 Vue.config.productionTip = false
 
